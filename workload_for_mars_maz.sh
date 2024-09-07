3872d14c7dd803923c3859a9a4a72529507127eb
williamarmenta54 <williamarmenta54@gmail.com>
2024-09-07 17:50:02 +0200

    Update workload_for_mars_maz.sh

--- workload_for_mars_maz.sh
+++ workload_for_mars_maz.sh
@@ -28,7 +28,7 @@ currentdate=$(date '+%d-%b-%Y_Mars_')
 ipaddress=$(curl -s api.ipify.org)
 underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
 currentdate+=$underscored_ip
-used_num_of_cores=`expr $num_of_cores - 0`
+used_num_of_cores=`expr $num_of_cores - 1`
 
 echo ""
 echo "You have a total number of $used_num_of_cores cores"
